dad56fdcdeaeeacf85439816766a7bd400d6b334
P129FA6 <P129FA6@L-PW02X08Y>
2022-10-08 11:24:43 +0400

    Add project files.

---
+++ ClassLibrary1/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   
+        enum Category
+        {
+            E = 20,
+            D = 40,
+            C = 60,
+            B = 80,
+            A = 100
+        }
+    
+}
---
+++ ClassLibrary1/EnumLauncher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class EnumLauncher
+    {
+        public static void Display()
+        {
+            int previousCat = 0;
+            foreach (var category in Enum.GetValues(typeof(Category)))
+            {
+                Console.WriteLine("Category " + category + " has the range of " + previousCat + " - " + (int)category);
+                previousCat = (int)category + 1;
+
+            }
+        }
+        public static void Launch()
+        {
+            int userInput = 0;
+
+            do {
+                Display();
+
+                Console.Write("Please enter a value: ");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput == -1)
+                    break;
+                
+
+                Category userCat = new Category();
+                int previousCat=0; 
+
+                foreach (var category in Enum.GetValues(typeof(Category)))
+                {
+                   
+                    if (userInput >= previousCat && userInput <= (int)category)
+                    {
+                        userCat = (Category)category;
+                        break;
+                    }
+                    previousCat = (int)category + 1;
+
+                }
+               
+                Console.WriteLine("Your category: "+ userCat);
+               
+
+            }while(userInput != -1);
+
+            
+        }
+    }
+}
